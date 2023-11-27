d2ef9529f7fd89c9d21eaead8ee513d93754c7af
Orpheus Lummis <o@orpheuslummis.info>
2023-11-27 21:20:54 +0545

    feat: content

--- src/navigation.js
+++ src/navigation.js
@@ -1,183 +1,157 @@
-import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+import { getPermalink, getAsset } from './utils/permalinks';
 
-export const headerData = {
-  links: [
-    {
-      text: 'Homes',
-      links: [
-        {
-          text: 'SaaS',
-          href: getPermalink('/homes/saas'),
-        },
-        {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
-        },
-        {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
-        },
-        {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
-        },
-      ],
-    },
-    {
-      text: 'Pages',
-      links: [
-        {
-          text: 'Features (Anchor Link)',
-          href: getPermalink('/#features'),
-        },
-        {
-          text: 'Services',
-          href: getPermalink('/services'),
-        },
-        {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
-        },
-        {
-          text: 'About us',
-          href: getPermalink('/about'),
-        },
-        {
-          text: 'Contact',
-          href: getPermalink('/contact'),
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
-        },
-      ],
-    },
-    {
-      text: 'Landing',
-      links: [
-        {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
-        },
-        {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
-        },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
-      ],
-    },
-    {
-      text: 'Blog',
-      links: [
-        {
-          text: 'Blog List',
-          href: getBlogPermalink(),
-        },
-        {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
-        },
-      ],
-    },
-    {
-      text: 'Widgets',
-      href: '#',
-    },
-  ],
-  actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
-};
+// export const headerData = {
+//   links: [
+//     {
+//       text: 'Homes',
+//       links: [
+//         {
+//           text: 'SaaS',
+//           href: getPermalink('/homes/saas'),
+//         },
+//         {
+//           text: 'Startup',
+//           href: getPermalink('/homes/startup'),
+//         },
+//         {
+//           text: 'Mobile App',
+//           href: getPermalink('/homes/mobile-app'),
+//         },
+//         {
+//           text: 'Personal',
+//           href: getPermalink('/homes/personal'),
+//         },
+//       ],
+//     },
+//     {
+//       text: 'Pages',
+//       links: [
+//         {
+//           text: 'Features (Anchor Link)',
+//           href: getPermalink('/#features'),
+//         },
+//         {
+//           text: 'Services',
+//           href: getPermalink('/services'),
+//         },
+//         {
+//           text: 'Pricing',
+//           href: getPermalink('/pricing'),
+//         },
+//         {
+//           text: 'About us',
+//           href: getPermalink('/about'),
+//         },
+//         {
+//           text: 'Contact',
+//           href: getPermalink('/contact'),
+//         },
+//         {
+//           text: 'Terms',
+//           href: getPermalink('/terms'),
+//         },
+//         {
+//           text: 'Privacy policy',
+//           href: getPermalink('/privacy'),
+//         },
+//       ],
+//     },
+//     {
+//       text: 'Landing',
+//       links: [
+//         {
+//           text: 'Lead Generation',
+//           href: getPermalink('/landing/lead-generation'),
+//         },
+//         {
+//           text: 'Long-form Sales',
+//           href: getPermalink('/landing/sales'),
+//         },
+//         {
+//           text: 'Click-Through',
+//           href: getPermalink('/landing/click-through'),
+//         },
+//         {
+//           text: 'Product Details (or Services)',
+//           href: getPermalink('/landing/product'),
+//         },
+//         {
+//           text: 'Coming Soon or Pre-Launch',
+//           href: getPermalink('/landing/pre-launch'),
+//         },
+//         {
+//           text: 'Subscription',
+//           href: getPermalink('/landing/subscription'),
+//         },
+//       ],
+//     },
+//     {
+//       text: 'Blog',
+//       links: [
+//         {
+//           text: 'Blog List',
+//           href: getBlogPermalink(),
+//         },
+//         {
+//           text: 'Article',
+//           href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
+//         },
+//         {
+//           text: 'Article (with MDX)',
+//           href: getPermalink('markdown-elements-demo-post', 'post'),
+//         },
+//         {
+//           text: 'Category Page',
+//           href: getPermalink('tutorials', 'category'),
+//         },
+//         {
+//           text: 'Tag Page',
+//           href: getPermalink('astro', 'tag'),
+//         },
+//       ],
+//     },
+//     {
+//       text: 'Widgets',
+//       href: '#',
+//     },
+//   ],
+//   actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
+// };
 
 export const footerData = {
   links: [
     {
-      title: 'Product',
+      title: 'Contact',
       links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
+        { text: 'Email', href: 'mailto:team@coincidence.network' },
+        {
+          text: 'Anonymous feedback',
+          href: 'https://docs.google.com/forms/d/e/1FAIpQLSeYM5u7k2G3ynVysPw8CD2IsuOX92i5jAcIRbr0vu0yGWWwxQ/viewform?usp=sf_link',
+        },
       ],
     },
     {
-      title: 'Platform',
+      title: 'Community',
       links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
-      ],
-    },
-    {
-      title: 'Support',
-      links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
+        { text: 'Matrix', href: 'https://matrix.to/#/#coincidence:matrix.org' },
+        { text: 'Twitter', href: 'https://twitter.com/TeamCoincidence' },
+        { text: 'Github', href: 'https://github.com/CoincidenceNetwork/' },
       ],
     },
     {
       title: 'Company',
       links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
+        { text: 'Manifesto', href: '/manifesto' },
+        { text: 'About', href: '/manifesto' },
       ],
     },
   ],
-  secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
-  ],
+  secondaryLinks: [{ text: 'Terms of service', href: getPermalink('/terms') }],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://twitter.com/TeamCoincidence' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/CoincidenceNetwork/' },
   ],
   footNote: `
-    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
-    Made by <a class="text-blue-600 hover:underline dark:text-gray-200" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+  Coincidence (c) 2023
   `,
 };
--- src/pages/terms.md
+++ src/pages/terms.md
@@ -3,7 +3,7 @@ title: 'Terms and Conditions'
 layout: '~/layouts/MarkdownLayout.astro'
 ---
 
-_Last updated_: January 06, 2023
+_Last updated_: November 27, 2023
 
 Please read these terms and conditions carefully before using Our Service.
 
@@ -21,7 +21,7 @@ For the purposes of these Terms and Conditions:
 
 - **Country** refers to: California, United States
 
-- **Company** (referred to as either "the Company", "We", "Us" or "Our" in this Agreement) refers to AstroWind LLC, 1 Cupertino, CA 95014.
+- **Company** (referred to as either "the Company", "We", "Us" or "Our" in this Agreement) refers to Coincidence.
 
 - **Device** means any device that can access the Service such as a computer, a cellphone or a digital tablet.
 
@@ -31,7 +31,7 @@ For the purposes of these Terms and Conditions:
 
 - **Third-party Social Media Service** means any services or content (including data, information, products or services) provided by a third-party that may be displayed, included or made available by the Service.
 
-- **Website** refers to AstroWind, accessible from [https://astrowind.vercel.app](https://astrowind.vercel.app)
+- **Website** refers to Coincidence, accessible from [coincidence.network](https://coincidence.network)
 
 - **You** means the individual accessing or using the Service, or the company, or other legal entity on behalf of which such individual is accessing or using the Service, as applicable.
 
@@ -117,4 +117,5 @@ By continuing to access or use Our Service after those revisions become effectiv
 
 If you have any questions about these Terms and Conditions, You can contact us:
 
-- By email: somecoolemail@domain.com
+- By email: team@coincidence.network
+- By matrix (encrypted): https://matrix.to/#/@orpheus:beeper.com
